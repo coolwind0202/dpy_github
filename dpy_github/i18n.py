0b6e80ed64edea3d456b7d0ecd45b2bb14801f6f
涼宮 <51913600+coolwind0202@users.noreply.github.com>
2020-10-24 14:30:39 +0900

    Update i18n.py

--- dpy_github/i18n.py
+++ dpy_github/i18n.py
@@ -1,10 +1,14 @@
 from discord import VoiceRegion, VerificationLevel, ContentFilter, NotificationLevel
 
 class PermissionTargets:
+    """abc.GuildChannel.overwrites等で返される辞書のキー群。
+    """    
     Role = "ロール"
     Member = "メンバー"
 
 class Channel:
+    """チャンネルの各属性に対応する日本語訳。
+    """    
     Name = "名前"
     ID = "ID"
     Position = "順番"
@@ -18,6 +22,8 @@ class Channel:
     UserLimit = "人数制限"
 
 class Role:
+    """ロールの各属性に対応する日本語訳。
+    """    
     Name = "名前"
     Hoist = "オンラインメンバーとは別にロールメンバーを表示"
     Position = "順番"
@@ -26,10 +32,14 @@ class Role:
     Color = "色（RGB値）"
 
 class Member:
+    """メンバーの各属性に対応する日本語訳。
+    """    
     Nick = "ニックネーム"
     Roles = "ロールのIDリスト"
 
 class Guild:
+    """サーバーの各属性に対応する日本語訳。
+    """    
     Name = "名前"
     Region = "サーバー地域"
     AfkTimeout = "非アクティブタイムアウト時間"
@@ -47,8 +57,18 @@ class Guild:
     PremiumSubscriptionCount = "サーバーのブースト数"
 
 class GeneralConverter:
+    """一般的なDiscordモデルの情報を整形するクラス。
+    """    
     @staticmethod
     def permission_name(perm_name: str) -> str:
+        """ロールやメンバーの権限名の日本語を返します。
+
+        Args:
+            perm_name (str): 調べたい権限名（英語）。
+
+        Returns:
+            str: 対応する日本語訳。もし見つからなければperm_nameがそのまま返ります。
+        """        
         perm_name_meaning_dict = {
             "create_instant_invite": "招待の作成",
             "kick_members": "メンバーをキック",
@@ -90,6 +110,14 @@ class GeneralConverter:
 
     @staticmethod
     def region_name(region: VoiceRegion) -> str:
+        """サーバー地域設定等におけるサーバー地域の日本語を返します。
+
+        Args:
+            region (str): 調べたい地域名（英語）。
+
+        Returns:
+            str: 対応する日本語訳。もし見つからなければregionがそのまま返ります。
+        """      
         region_name_meaning_dict = {
             VoiceRegion.amsterdam: "アムステルダム",
             VoiceRegion.brazil: "ブラジル",
@@ -118,6 +146,14 @@ class GeneralConverter:
 
     @staticmethod
     def verification_level(level: VerificationLevel) -> str:
+        """認証レベルの日本語を返します。
+
+        Args:
+            level (str): 調べたいレベル文字列（英語）。
+
+        Returns:
+            str: 対応する日本語訳。もし見つからなければlevelがそのまま返ります。
+        """      
         verification_level_meaning_dict = {
             VerificationLevel.low: "低：メール認証がされているアカウントのみ",
             VerificationLevel.medium: "中：Discordに登録してから5分以上経過したアカウントのみ",
@@ -131,6 +167,14 @@ class GeneralConverter:
 
     @staticmethod
     def explicit_content_filter(filter_: ContentFilter) -> str:
+        """メディアコンテンツフィルターを示す文字列の日本語を返します。
+
+        Args:
+            filter_ (str): 調べたいフィルター（英語）。
+
+        Returns:
+            str: 対応する日本語訳。もし見つからなければfilter_がそのまま返ります。
+        """      
         content_fileter_meaning_dict = {
             ContentFilter.disabled: "いかなるメディアコンテンツもスキャンしない",
             ContentFilter.no_role: "ロールのないメンバーのメディアコンテンツをスキャン",
@@ -140,6 +184,14 @@ class GeneralConverter:
 
     @staticmethod
     def notification_level(level: NotificationLevel) -> str:
+        """デフォルトの通知設定を示す英語の日本語を返します。
+
+        Args:
+            level (str): 調べたい通知設定用文字列（英語）。
+
+        Returns:
+            str: 対応する日本語訳。もし見つからなければlevelがそのまま返ります。
+        """      
         notification_levl_meaning_dict = {
             NotificationLevel.all_messages: "すべてのメッセージ",
             NotificationLevel.only_mentions: "@mentionsのみ"
@@ -147,18 +199,52 @@ class GeneralConverter:
         return notification_levl_meaning_dict.get(level, level)
 
 class BooleanConverter:
+    """一部のBool値を日本語に変換するクラス。
+    """    
     @staticmethod
     def yes_no(value: bool) -> str:
+        """いいえ, または はい のいずれかを利用したい文脈での変換で利用します。
+
+        Args:
+            value (bool): 変換したいbool。
+
+        Returns:
+            str: 対応する いいえ, または はい。
+        """        
         return ["いいえ", "はい"][value]
 
     @staticmethod
     def has_permission(value: bool) -> str:
+        """不可, または 可 のいずれかを利用したい文脈での変換で利用します。
+
+        Args:
+            value (bool): 変換したいbool。
+
+        Returns:
+            str: 対応する 不可, または 可。
+        """    
         return ["不可","可"][value]
 
     @staticmethod
     def do(value: bool) -> str:
+        """しない, または する のいずれかを利用したい文脈での変換で利用します。
+
+        Args:
+            value (bool): 変換したいbool。
+
+        Returns:
+            str: 対応する しない, または する。
+        """    
         return ["しない","する"][value]
 
     @staticmethod
     def valid(value: bool) -> str:
+        """無効, または 有効 のいずれかを利用したい文脈での変換で利用します。
+
+        Args:
+            value (bool): 変換したいbool。
+
+        Returns:
+            str: 対応する 無効, または 有効。
+        """    
         return ["無効","有効"][value]
